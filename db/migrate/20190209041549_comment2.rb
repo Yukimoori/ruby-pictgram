e7555d88945d3ad47667610278bbf1502df8894b
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-02-20 22:41:38 +0900

    heroku02202230

--- db/migrate/20190209041549_comment2.rb
+++
@@ -1,5 +0,0 @@
-class Comment2 < ActiveRecord::Migration[5.2]
-  def change
-    drop_table:commnets
-  end
-end
--- db/migrate/20190209043857_comment3.rb
+++
@@ -1,5 +0,0 @@
-class Comment3 < ActiveRecord::Migration[5.2]
-  def change
-    drop_table:comments
-  end
-end
